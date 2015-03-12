f90a95cfdf0918a7e614c547aca7dfd9d0cf8f94
denny <cdfq152313@gmail.com>
2015-03-12 20:34:24 +0800

    apt-get install(dependency) decouple from setup

---
+++ dependency.sh
@@ -0,0 +1,4 @@
+sudo apt-get install vim
+sudo apt-get install exuberant-ctags
+sudo apt-get install ack-grep
+sudo apt-get install silversearcher-ag
--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
-sudo apt-get install exuberant-ctags
-sudo apt-get install ack-grep
-sudo apt-get install silversearcher-ag
-
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 ln -f .vimrc ~/
 ln -f .bashrc ~/
