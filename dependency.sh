626192d69e9a7139db801a4db1574255a024860a
denny <cdfq152313@gmail.com>
2016-01-12 01:50:34 +0800

    adds dependency

--- dependency.sh
+++ dependency.sh
@@ -1,5 +1,6 @@
 sudo apt-get install vim
 sudo apt-get install exuberant-ctags
 sudo apt-get install ack-grep
+sudo apt-get install curl
 sudo apt-get install silversearcher-ag
 sudo apt-get install cscope
