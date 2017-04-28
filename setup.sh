806bebc18750dfb1464aedc1e41ebfa56907e758
Denny Cheng <denny.cheng@addweup.com>
2017-04-28 09:12:29 +0000

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 PWD=$(pwd)
 ln -fs $PWD/vimrc ~/.vimrc
-ln -fs $PWD/bash_prompt.sh ~/.bash_prompt
+echo ". $PWD/bash_prompt.sh" >> ~/.bashrc
